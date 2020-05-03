1ba17694c4996351f4cc24ee551e3f9ea4cebc89
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-03 18:40:11 +0200

    Add a module for sync storage

--- src/state/reducers.ts
+++ src/state/reducers.ts
@@ -1,12 +1,12 @@
-import storage from 'redux-persist/lib/storage'
 import { combineReducers } from 'redux'
 import { persistReducer } from 'redux-persist'
 
+import syncStorage from './syncStorage'
 import currentTabs from './currentTabs'
 import tabGroups from './tabGroups'
 
 export default persistReducer(
-  { key: 'root', storage, blacklist: ['currentTabs'] },
+  { key: 'root', storage: syncStorage(), blacklist: ['currentTabs'] },
   combineReducers({
     currentTabs,
     tabGroups,
