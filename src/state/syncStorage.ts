acce0d87c833c35b68eae34dd434e7dccbb7a21e
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-20 20:32:14 +0200

    Fix: Add logging and correct availability check for sync storage

--- src/state/syncStorage.ts
+++ src/state/syncStorage.ts
@@ -4,21 +4,19 @@ import { browser } from 'webextension-polyfill-ts'
 import storage from 'redux-persist/lib/storage'
 
 export default function syncStorage(): any {
-  console.log('initializing storage')
-
-  try {
-    browser.storage.sync.get({})
-  } catch (e) {
-    console.error(e)
-    return storage
-  }
-
-  console.log('sync storage')
+  browser.storage.sync
+    .get()
+    .then((data) => console.log('sync storage', data))
+    .catch((e) => {
+      console.log('local storage', e)
+      return storage
+    })
 
   return {
     async getItem(key: string): Promise<string> {
       const value = await browser.storage.sync.get(key)
       if (browser.runtime.lastError == null) {
+        console.log(`key ${key} retrieved`, value)
         // Chrome Storage returns the value in an Object of with its original key. Unwrap the
         // value from the returned Object to match the `getItem` API.
         return value[key]
@@ -29,14 +27,16 @@ export default function syncStorage(): any {
     async removeItem(key: string): Promise<any> {
       await browser.storage.sync.remove(key)
       if (browser.runtime.lastError == null) {
+        console.log(`key ${key} removed`)
         return
       } else {
         throw new Error()
       }
     },
     async setItem(key: string, value: any): Promise<any> {
-      browser.storage.sync.set({ [key]: value })
+      await browser.storage.sync.set({ [key]: value })
       if (browser.runtime.lastError == null) {
+        console.log(`key ${key} set`, value)
         return
       } else {
         throw new Error()
