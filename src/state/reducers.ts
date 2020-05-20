9dfb77906ff9aad22b60ae2c15f28adf52854d4f
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-20 20:34:23 +0200

    Add the redux-persist compression transform

--- src/state/reducers.ts
+++ src/state/reducers.ts
@@ -1,13 +1,21 @@
 import { combineReducers } from 'redux'
 import { persistReducer } from 'redux-persist'
 
+import createCompressor from '@src/lib/compress'
 import syncStorage from './syncStorage'
 import currentTabs from './currentTabs'
 import tabGroups from './tabGroups'
 import suggestions from './suggestions'
 
+const compressor = createCompressor({ whitelist: ['tabGroups'] } as any)
+
 export default persistReducer(
-  { key: 'root', storage: syncStorage(), blacklist: ['currentTabs', 'suggestions'] },
+  {
+    key: 'root',
+    storage: syncStorage(),
+    blacklist: ['currentTabs', 'suggestions'],
+    transforms: [compressor],
+  },
   combineReducers({
     currentTabs,
     suggestions,
