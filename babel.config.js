0e5ae4a1f16add7a13b5145b7f2eadbffe300e44
Roland Schläfli <rolandschlaefli@gmail.com>
2020-05-20 09:48:22 +0200

    Fix: Babel setup with additional presents, add core-js to babel/preset-env, install css optimization plugins for webpack

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: { version: 3, proposals: true },
+      },
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
   plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
     [
       'babel-plugin-import',
       {
