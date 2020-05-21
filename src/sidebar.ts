e8f206c5a91fb85eb6b25d0a91270e0d07fa9fe9
Roland Schläfli <rolandschlaefli@gmail.com>
2020-05-21 22:44:42 +0200

    Bind keyboard shortcuts for sidebar toggling

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -1,8 +1,9 @@
 // ref: https://github.com/lusakasa/saka/blob/master/src/content_script/toggle_saka.js
 
-import optionsStorage from './optionsStorage'
-
 import { browser } from 'webextension-polyfill-ts'
+import Mousetrap from 'mousetrap'
+
+import optionsStorage from './optionsStorage'
 
 const sidebar = document.createElement('iframe')
 sidebar.id = 'tabs-sidebar'
@@ -12,15 +13,24 @@ const sidebarWrapper = document.createElement('div')
 sidebarWrapper.id = 'tabs-sidebar-wrapper'
 sidebarWrapper.appendChild(sidebar)
 
-const sidebarToggle = document.createElement('button')
-sidebarToggle.id = 'tabs-sidebar-toggle'
-sidebarToggle.addEventListener('click', (e) => {
-  e.preventDefault()
+const toggleSidebar = (): void => {
   if (sidebarWrapper.className.includes('open')) {
     sidebarWrapper.setAttribute('class', '')
   } else {
     sidebarWrapper.setAttribute('class', 'open')
   }
+}
+
+Mousetrap.bind(['command+shift+x', 'ctrl+shift+x'], function () {
+  toggleSidebar()
+  return false
+})
+
+const sidebarToggle = document.createElement('button')
+sidebarToggle.id = 'tabs-sidebar-toggle'
+sidebarToggle.addEventListener('click', (e) => {
+  e.preventDefault()
+  toggleSidebar()
 })
 
 sidebarWrapper.appendChild(sidebarToggle)
