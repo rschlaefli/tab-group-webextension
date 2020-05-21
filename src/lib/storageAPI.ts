4428bdf07f8c99a1a5ec6f19d3f0bde594c3de37
Roland Schläfli <rolandschlaefli@gmail.com>
2020-05-21 15:18:27 +0200

    Implement periodic sync from local storage mirror

---
+++ src/lib/storageAPI.ts
@@ -0,0 +1,39 @@
+// ref: https://github.com/ssorallen/redux-persist-webextension-storage/blob/master/src/createStorage.js
+
+import { Storage } from 'redux-persist'
+
+function createStorageAPI(type: 'sync' | 'local'): Storage {
+  return {
+    async getItem(key: string): Promise<string> {
+      const value = await browser.storage[type].get(key)
+      if (browser.runtime.lastError == null) {
+        // Chrome Storage returns the value in an Object of with its original key. Unwrap the
+        // value from the returned Object to match the `getItem` API.
+        return value[key]
+      } else {
+        throw new Error()
+      }
+    },
+    async removeItem(key: string): Promise<any> {
+      await browser.storage[type].remove(key)
+      if (browser.runtime.lastError == null) {
+        return
+      } else {
+        throw new Error()
+      }
+    },
+    async setItem(key: string, value: any): Promise<void> {
+      await browser.storage[type].set({ [key]: value })
+      if (browser.runtime.lastError == null) {
+        return
+      } else {
+        throw new Error()
+      }
+    },
+  }
+}
+
+export default {
+  local: createStorageAPI('local'),
+  sync: createStorageAPI('sync'),
+}
--- src/state/syncStorage.ts
+++ src/state/syncStorage.ts
@@ -1,7 +1,9 @@
 // ref: https://github.com/ssorallen/redux-persist-webextension-storage/blob/master/src/createStorage.js
 
 import { browser } from 'webextension-polyfill-ts'
-import localForage from 'localforage'
+import { Storage } from 'redux-persist'
+
+import StorageAPI from '../lib/storageAPI'
 
 // shim requestIdleCallback in case of it not being available
 // ref: https://developer.mozilla.org/en-US/docs/Web/API/Background_Tasks_API
@@ -20,32 +22,99 @@ window.requestIdleCallback =
     }, 1)
   }
 
-// localForage.config({
-//   driver: [localForage.INDEXEDDB],
-// })
-
-export default function syncStorage(): any {
+export default function syncStorage(): Storage {
   browser.storage.sync
     .get()
     .then(async (data) => {
-      console.log('sync storage', data)
+      console.log('> sync storage available', data)
 
-      // initialize the local storage from sync
-      await localForage.clear()
-      await localForage.setItem('persist:root', data['persist:root'])
+      const lastUpdate = await StorageAPI.local.getItem('lastUpdate')
+
+      if (!lastUpdate || (data.lastUpdate && lastUpdate < data.lastUpdate)) {
+        await Promise.all([
+          StorageAPI.local.setItem('persist:root', data['persist:root']),
+          StorageAPI.local.setItem('lastUpdate', data.lastUpdate),
+        ])
+      }
+
+      // setup a listener to watch for sync storage changes
+      // browser.storage.onChanged.addListener(async (changes, areaName) => {
+      //   console.log('> browser storage changed', areaName, changes)
+
+      //   // if lastUpdate changed in the sync storage after initialization, evaluate further
+      //   if (areaName === 'sync' && changes['lastUpdate']) {
+      //     const lastSync = localForage.getItem('lastSync')
+
+      //     // if the last sync was before the last update, override the local storage
+      //     if (!lastSync || data.lastUpdate < changes['lastUpdate'] || lastSync < changes['lastUpdate']) {
+      //       await localForage.clear()
+      //       await localForage.setItem('persist:root', data['persist:root'])
+      //       await localForage.setItem('lastSync', Date.now())
+      //     }
+      //   }
+      // })
     })
     .catch((e) => {
-      console.log('local storage', e)
-      return localForage
+      console.log('> fallback to local storage', e)
+      return StorageAPI.local
     })
 
   // mirror local storage to sync on a regular schedule
   async function syncLocalStorage(): Promise<void> {
-    const localData = await localForage.getItem('persist:root')
-    await browser.storage.sync.set({ 'persist:root': localData })
-    console.log('> mirrored local storage to sync')
+    const lastUpdateLocal: number = await StorageAPI.local.getItem('lastUpdate')
+    const lastUpdate = await StorageAPI.sync.getItem('lastUpdate')
+
+    console.log(lastUpdate, lastUpdateLocal)
+
+    // if the local version is more current than the one on sync
+    // we want to upload our changes
+    if (lastUpdateLocal > lastUpdate) {
+      const localData = await StorageAPI.local.getItem('persist:root')
+
+      await Promise.all([
+        StorageAPI.sync.setItem('persist:root', localData),
+        StorageAPI.sync.setItem('lastUpdate', lastUpdateLocal),
+      ])
+
+      return
+    }
+
+    // if the sync version is more current than the one locally
+    // we want to download remote changes
+    if (lastUpdateLocal < lastUpdate) {
+      const syncData = await StorageAPI.sync.getItem('persist:root')
+
+      await Promise.all([
+        StorageAPI.local.setItem('persist:root', syncData),
+        StorageAPI.local.setItem('lastUpdate', lastUpdate),
+      ])
+
+      return
+    }
+
+    // TODO: evaluate the risk of inconsistencies (if groups edited on two devices simultaneously)
+    // if the update timestamps are the same, we could either compare hashes and merge states
+    // or we could simply do nothing and wait for the next update (could get inconsistencies)
   }
   setInterval(() => window.requestIdleCallback(syncLocalStorage, { timeout: 5000 }), 10000)
 
-  return localForage
+  return {
+    getItem: StorageAPI.local.getItem,
+    async removeItem(key: string): Promise<void> {
+      await Promise.all([
+        StorageAPI.local.removeItem(key),
+        StorageAPI.local.setItem('lastUpdate', Date.now()),
+      ])
+    },
+    async setItem(key: string, value: any): Promise<void> {
+      if (key === 'persist:root') {
+        await Promise.all([
+          StorageAPI.local.setItem(key, value),
+          StorageAPI.local.setItem('lastUpdate', Date.now()),
+        ])
+      } else {
+        StorageAPI.local.setItem(key, value)
+      }
+    },
+  }
 }
