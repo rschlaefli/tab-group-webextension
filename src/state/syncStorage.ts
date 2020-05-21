45b1daf3dffbd7dda1a9702d0747dffcee0b0ba6
Roland Schläfli <rolandschlaefli@gmail.com>
2020-05-21 13:36:42 +0200

    Setup periodic state mirroring with localForage

--- src/state/syncStorage.ts
+++ src/state/syncStorage.ts
@@ -1,46 +1,51 @@
 // ref: https://github.com/ssorallen/redux-persist-webextension-storage/blob/master/src/createStorage.js
 
 import { browser } from 'webextension-polyfill-ts'
-import storage from 'redux-persist/lib/storage'
+import localForage from 'localforage'
+
+// shim requestIdleCallback in case of it not being available
+// ref: https://developer.mozilla.org/en-US/docs/Web/API/Background_Tasks_API
+window.requestIdleCallback =
+  window.requestIdleCallback ||
+  function (handler): any {
+    const startTime = Date.now()
+
+    return setTimeout(function () {
+      handler({
+        didTimeout: false,
+        timeRemaining: function () {
+          return Math.max(0, 50.0 - (Date.now() - startTime))
+        },
+      })
+    }, 1)
+  }
+
+// localForage.config({
+//   driver: [localForage.INDEXEDDB],
+// })
 
 export default function syncStorage(): any {
   browser.storage.sync
     .get()
-    .then((data) => console.log('sync storage', data))
+    .then(async (data) => {
+      console.log('sync storage', data)
+
+      // initialize the local storage from sync
+      await localForage.clear()
+      await localForage.setItem('persist:root', data['persist:root'])
+    })
     .catch((e) => {
       console.log('local storage', e)
-      return storage
+      return localForage
     })
 
-  return {
-    async getItem(key: string): Promise<string> {
-      const value = await browser.storage.sync.get(key)
-      if (browser.runtime.lastError == null) {
-        console.log(`key ${key} retrieved`, value)
-        // Chrome Storage returns the value in an Object of with its original key. Unwrap the
-        // value from the returned Object to match the `getItem` API.
-        return value[key]
-      } else {
-        throw new Error()
-      }
-    },
-    async removeItem(key: string): Promise<any> {
-      await browser.storage.sync.remove(key)
-      if (browser.runtime.lastError == null) {
-        console.log(`key ${key} removed`)
-        return
-      } else {
-        throw new Error()
-      }
-    },
-    async setItem(key: string, value: any): Promise<any> {
-      await browser.storage.sync.set({ [key]: value })
-      if (browser.runtime.lastError == null) {
-        console.log(`key ${key} set`, value)
-        return
-      } else {
-        throw new Error()
-      }
-    },
+  // mirror local storage to sync on a regular schedule
+  async function syncLocalStorage(): Promise<void> {
+    const localData = await localForage.getItem('persist:root')
+    await browser.storage.sync.set({ 'persist:root': localData })
+    console.log('> mirrored local storage to sync')
   }
+  setInterval(() => window.requestIdleCallback(syncLocalStorage, { timeout: 5000 }), 10000)
+
+  return localForage
 }
