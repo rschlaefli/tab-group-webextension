b10f95055b12ef194e41b776a1cb859ac9738f1f
Roland Schläfli <rolandschlaefli@gmail.com>
2020-05-22 14:51:14 +0200

    Fix: Only use redux-logger in development

--- src/state/configureStore.ts
+++ src/state/configureStore.ts
@@ -1,6 +1,5 @@
 import { alias } from 'webext-redux'
 import { configureStore, Store, getDefaultMiddleware } from '@reduxjs/toolkit'
-import logger from 'redux-logger'
 import {
   persistStore,
   Persistor,
@@ -17,20 +16,27 @@ import rootReducer from './reducers'
 
 export type RootState = ReturnType<typeof rootReducer>
 
+const middleware = [
+  alias(aliases),
+  ...getDefaultMiddleware<RootState>({
+    serializableCheck: {
+      // we need to disable these to prevent issues in conjunction with redux-persist
+      // ref: https://github.com/rt2zz/redux-persist/issues/988
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  } as any),
+]
+
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { logger } = require('redux-logger')
+  middleware.push(logger)
+}
+
 export default ({ persistence = true }: any): { store: Store; persistor?: Persistor } => {
   const store = configureStore({
     reducer: rootReducer,
-    middleware: [
-      alias(aliases),
-      ...getDefaultMiddleware<RootState>({
-        serializableCheck: {
-          // we need to disable these to prevent issues in conjunction with redux-persist
-          // ref: https://github.com/rt2zz/redux-persist/issues/988
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      } as any),
-      logger,
-    ] as const,
+    middleware,
   })
 
   if (persistence) {
