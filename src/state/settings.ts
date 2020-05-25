61a055f51ca8b1a64c85657b0e7cd8555b318b5f
Roland Schläfli <rolandschlaefli@gmail.com>
2020-05-25 18:36:04 +0200

    Extend settings reducer with options from NPM module

--- src/state/settings.ts
+++ src/state/settings.ts
@@ -1,36 +1,92 @@
 import { createSlice, createAction, createAsyncThunk } from '@reduxjs/toolkit'
+import { browser } from 'webextension-polyfill-ts'
 
-import { AppDispatch } from '@src/background'
+import { performBrowserActionSafe, getBrowserSafe } from '@src/lib/utils'
+import { AppDispatch, bootstrap } from '@src/background'
 import { RootState } from './configureStore'
-import { getBrowserSafe } from '@src/lib/utils'
+import { processSettings } from '@src/lib/listeners'
 
 const settingsSlice = createSlice({
   name: 'settings',
   initialState: {
-    focusModeEnabled: false,
+    isDebugLoggingEnabled: true,
+    isHeuristicsBackendEnabled: false,
+    isFocusModeEnabled: false,
+    tutorialProgress: 0,
   },
   reducers: {
+    updateIsDebugLoggingEnabled(state, action): void {
+      state.isDebugLoggingEnabled = action.payload
+    },
+    updateIsHeuristicsBackendEnabled(state, action): void {
+      state.isHeuristicsBackendEnabled = action.payload
+    },
     toggleFocusMode(state): void {
-      state.focusModeEnabled = !state.focusModeEnabled
+      state.isFocusModeEnabled = !state.isFocusModeEnabled
+    },
+    resetTutorialProgress(state): void {
+      state.tutorialProgress = 0
+    },
+    updateTutorialProgress(state, action): void {
+      state.tutorialProgress = action.payload
     },
   },
 })
 
 const { actions, reducer } = settingsSlice
-export const { toggleFocusMode } = actions
+export const {
+  toggleFocusMode,
+  resetTutorialProgress,
+  updateIsDebugLoggingEnabled,
+  updateIsHeuristicsBackendEnabled,
+  updateTutorialProgress,
+} = actions
 export default reducer
 
 // THUNKS
 export const openOptionsPage = createAsyncThunk(
-  'tabGroups/closeTabGroup',
+  'settings/openOptionsPage',
   async (): Promise<void> => {
-    const browser = await getBrowserSafe()
-    browser.runtime.openOptionsPage()
+    await performBrowserActionSafe(async (browser) => {
+      await browser.runtime.openOptionsPage()
+    })
+  }
+)
+
+export const toggleDebugLogging = createAsyncThunk<
+  void,
+  void,
+  { dispatch: AppDispatch; state: RootState }
+>(
+  'settings/toggleDebugLogging',
+  async (_, thunkAPI): Promise<void> => {
+    const state = thunkAPI.getState()
+    thunkAPI.dispatch(updateIsDebugLoggingEnabled(!state.settings.isDebugLoggingEnabled))
+    processSettings(thunkAPI)({ isDebugLoggingEnabled: !state.settings.isDebugLoggingEnabled })
+  }
+)
+
+export const toggleHeuristicsBackend = createAsyncThunk<
+  void,
+  void,
+  { dispatch: AppDispatch; state: RootState }
+>(
+  'settings/toggleHeuristicsBackend',
+  async (_, thunkAPI): Promise<void> => {
+    const state = thunkAPI.getState()
+    thunkAPI.dispatch(updateIsHeuristicsBackendEnabled(!state.settings.isHeuristicsBackendEnabled))
+    processSettings(thunkAPI)({
+      isHeuristicsBackendEnabled: !state.settings.isHeuristicsBackendEnabled,
+    })
   }
 )
 
 // ALIASES
-export const openOptionsPageAlias = createAction('settings/openOptionsPage')
+export const openOptionsPageAlias = createAction('settings/openOptionsPageAlias')
+export const toggleDebugLoggingAlias = createAction('settings/toggleDebugLoggingAlias')
+export const toggleHeuristicsBackendAlias = createAction('settings/toggleHeuristicsBackendAlias')
 export const settingsAliases = {
   [openOptionsPageAlias.type]: openOptionsPage,
+  [toggleDebugLoggingAlias.type]: toggleDebugLogging,
+  [toggleHeuristicsBackendAlias.type]: toggleHeuristicsBackend,
 }
