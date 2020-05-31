eebe84d4d3c2e7579a3a90cd7e909871358cd50d
Roland Schläfli <rolandschlaefli@gmail.com>
2020-05-31 20:58:07 +0200

    Add existing tab group interactions

--- src/components/tabs/TabGroup.tsx
+++ src/components/tabs/TabGroup.tsx
@@ -6,6 +6,8 @@ import { Save, Delete, Launch, ArrowDropDown, ArrowDropUp, Close } from '@materi
 import Input from '../common/Input'
 import Tab from './Tab'
 import { ITab } from '@src/types/Extension'
+import useContextMenu from '@src/lib/useContextMenu'
+import { Menu, MenuItem } from '@material-ui/core'
 
 interface IProps {
   id: string
@@ -48,121 +50,155 @@ function TabGroup({
   onOpenCurrentTab,
   onCloseTabGroup,
 }: IProps): React.ReactElement {
+  const {
+    isContextMenuOpen,
+    contextAnchorPosition,
+    handleOpenContextMenu,
+    handleCloseContextMenu,
+  } = useContextMenu()
+
   return (
-    <Droppable ignoreContainerClipping droppableId={id} isDropDisabled={isSuggested}>
-      {(provided: DroppableProvided, snapshot: DroppableStateSnapshot): React.ReactElement => (
-        <div
-          className={clsx(
-            'flex-1 mb-2 border border-solid md:mr-2 md:last:mr-0 md:max-w-xxs md:min-w-xxs dark:border-gray-500',
-            isSuggested && ''
-          )}
-          ref={provided.innerRef}
-          {...provided.droppableProps}
-          style={getListStyle(snapshot.isDraggingOver)}
-        >
-          <div className="flex flex-row items-center justify-between px-2 py-1 bg-gray-100 dark:text-gray-900 dark:bg-gray-700">
-            <button
-              className={clsx(
-                'mr-2 text-sm md:hidden text-gray-600 dark:text-gray-400',
-                tabs.length === 0 && 'text-gray-400 dark:text-gray-600 cursor-default',
-                isSuggested && 'hidden'
-              )}
-              disabled={tabs.length === 0}
-              onClick={onCollapseGroup}
+    <>
+      <Droppable ignoreContainerClipping droppableId={id} isDropDisabled={isSuggested}>
+        {(provided: DroppableProvided, snapshot: DroppableStateSnapshot): React.ReactElement => (
+          <div
+            className={clsx(
+              'flex-1 mb-2 border border-solid md:mr-2 md:last:mr-0 md:max-w-xxs md:min-w-xxs dark:border-gray-500',
+              isSuggested && ''
+            )}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            style={getListStyle(snapshot.isDraggingOver)}
+          >
+            <div
+              className="flex flex-row items-center justify-between px-2 py-1 bg-gray-100 dark:text-gray-900 dark:bg-gray-700"
+              onContextMenu={handleOpenContextMenu}
             >
-              {isCollapsed ? (
-                <ArrowDropDown fontSize="inherit" />
-              ) : (
-                <ArrowDropUp fontSize="inherit" />
-              )}
-            </button>
+              <button
+                className={clsx(
+                  'mr-2 text-sm md:hidden text-gray-600 dark:text-gray-400',
+                  tabs.length === 0 && 'text-gray-400 dark:text-gray-600 cursor-default',
+                  isSuggested && 'hidden'
+                )}
+                disabled={tabs.length === 0}
+                onClick={onCollapseGroup}
+              >
+                {isCollapsed ? (
+                  <ArrowDropDown fontSize="inherit" />
+                ) : (
+                  <ArrowDropUp fontSize="inherit" />
+                )}
+              </button>
+              <h1 className="w-full mr-2 text-xs font-bold text-gray-600 dark:text-gray-400">
+                {isReadOnly ? name : <Input fullWidth value={name} onChange={onChangeGroupName} />}
+              </h1>
+              <div className="flex flex-row">
+                {(!isReadOnly || isSuggested) && (
+                  <button
+                    key="open"
+                    className="text-sm text-gray-600 dark:text-gray-400"
+                    onClick={onOpenTabGroup}
+                    title="open group"
+                  >
+                    <Launch fontSize="inherit" />
+                  </button>
+                )}
 
-            <h1 className="w-full mr-2 text-xs font-bold text-gray-600 dark:text-gray-400">
-              {isReadOnly ? name : <Input fullWidth value={name} onChange={onChangeGroupName} />}
-            </h1>
+                {!isReadOnly && [
+                  <button
+                    key="close"
+                    className="ml-2 text-sm text-gray-600 dark:text-gray-400"
+                    onClick={onCloseTabGroup}
+                    title="close group"
+                  >
+                    <Close fontSize="inherit" />
+                  </button>,
+                  <button
+                    key="remove"
+                    className="ml-2 text-sm text-gray-600 dark:text-gray-400"
+                    onClick={onRemoveTabGroup}
+                    title="remove group"
+                  >
+                    <Delete fontSize="inherit" />
+                  </button>,
+                ]}
 
-            <div className="flex flex-row">
-              {(!isReadOnly || isSuggested) && (
-                <button
-                  key="open"
-                  className="text-sm text-gray-600 dark:text-gray-400"
-                  onClick={onOpenTabGroup}
-                  title="open group"
-                >
-                  <Launch fontSize="inherit" />
-                </button>
-              )}
-
-              {!isReadOnly && [
-                <button
-                  key="close"
-                  className="ml-2 text-sm text-gray-600 dark:text-gray-400"
-                  onClick={onCloseTabGroup}
-                  title="close group"
-                >
-                  <Close fontSize="inherit" />
-                </button>,
-                <button
-                  key="remove"
-                  className="ml-2 text-sm text-gray-600 dark:text-gray-400"
-                  onClick={onRemoveTabGroup}
-                  title="remove group"
-                >
-                  <Delete fontSize="inherit" />
-                </button>,
-              ]}
-
-              {isSuggested && [
-                <button
-                  disabled
-                  key="discard"
-                  className="ml-2 mr-2 text-sm text-gray-400 dark:text-gray-600"
-                  onClick={() => null}
-                  title="discard suggestion"
-                >
-                  <Delete fontSize="inherit" />
-                </button>,
-                <button
-                  key="save"
-                  className="text-sm text-gray-600 dark:text-gray-400"
-                  onClick={onSaveSuggestion}
-                  title="save suggestion"
-                >
-                  <Save fontSize="inherit" />
-                </button>,
-              ]}
+                {isSuggested && [
+                  <button
+                    disabled
+                    key="discard"
+                    className="ml-2 mr-2 text-sm text-gray-400 dark:text-gray-600"
+                    onClick={() => null}
+                    title="discard suggestion"
+                  >
+                    <Delete fontSize="inherit" />
+                  </button>,
+                  <button
+                    key="save"
+                    className="text-sm text-gray-600 dark:text-gray-400"
+                    onClick={onSaveSuggestion}
+                    title="save suggestion"
+                  >
+                    <Save fontSize="inherit" />
+                  </button>,
+                ]}
+              </div>
             </div>
-          </div>
 
-          <div className={clsx('min-h-8', tabs.length > 0 && isCollapsed && 'hidden', 'md:block')}>
-            {tabs
-              .filter((tab) => typeof tab.id !== 'undefined')
-              .map((tab: ITab, index: number) => {
-                const uniqueId = `${tab.id}-${tab.hash}-${id}`
-                return (
-                  <Tab
-                    uniqueId={uniqueId}
-                    key={uniqueId}
-                    title={tab.title}
-                    index={index}
-                    url={tab.url}
-                    faviconUrl={tab.favIconUrl}
-                    windowId={tab.windowId}
-                    isReadOnly={isReadOnly}
-                    isOpen={!!tab.hash && currentTabs && currentTabs.includes(tab.hash)}
-                    isSuggested={isSuggested}
-                    onRemoveTab={onRemoveTab && onRemoveTab(index)}
-                    onCloseTab={onCloseTab && onCloseTab(tab.id as number)}
-                    onOpenCurrentTab={!!tab.hash && onOpenCurrentTab && onOpenCurrentTab(tab.hash)}
-                  />
-                )
-              })}
-          </div>
+            <div
+              className={clsx('min-h-8', tabs.length > 0 && isCollapsed && 'hidden', 'md:block')}
+            >
+              {tabs
+                .filter((tab) => typeof tab.id !== 'undefined')
+                .map((tab: ITab, index: number) => {
+                  const uniqueId = `${tab.id}-${tab.hash}-${id}`
+                  return (
+                    <Tab
+                      uniqueId={uniqueId}
+                      key={uniqueId}
+                      title={tab.title}
+                      index={index}
+                      url={tab.url}
+                      faviconUrl={tab.favIconUrl}
+                      windowId={tab.windowId}
+                      isReadOnly={isReadOnly}
+                      isOpen={!!tab.hash && currentTabs && currentTabs.includes(tab.hash)}
+                      isSuggested={isSuggested}
+                      onRemoveTab={onRemoveTab && onRemoveTab(index)}
+                      onCloseTab={onCloseTab && onCloseTab(tab.id as number)}
+                      onOpenCurrentTab={
+                        !!tab.hash && onOpenCurrentTab && onOpenCurrentTab(tab.hash)
+                      }
+                    />
+                  )
+                })}
+            </div>
 
-          {provided.placeholder}
-        </div>
-      )}
-    </Droppable>
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+
+      <Menu
+        keepMounted
+        open={isContextMenuOpen}
+        onClose={handleCloseContextMenu}
+        anchorReference="anchorPosition"
+        anchorPosition={contextAnchorPosition}
+      >
+        {(!isReadOnly || isSuggested) && (
+          <MenuItem onClick={onOpenTabGroup}>Open Tab Group</MenuItem>
+        )}
+        {!isReadOnly && [
+          <MenuItem key="close" onClick={onCloseTabGroup}>
+            Close Tab Group
+          </MenuItem>,
+          <MenuItem key="remove" onClick={onRemoveTabGroup}>
+            Remove Tab Group
+          </MenuItem>,
+        ]}
+      </Menu>
+    </>
   )
 }
 
