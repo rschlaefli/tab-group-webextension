2e40ff8ef9301b9d972012035b81500584784795
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-31 18:51:07 +0200

    Basic table in MUI with react-table

--- src/data.tsx
+++ src/data.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import withProxyStore from './lib/withProxyStore'
 import Data from './pages/Data'
 
 //load tailwind styling and reset css
--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -1,7 +1,74 @@
 import React from 'react'
+import { useTable } from 'react-table'
+import { Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core'
+
+import Layout from '@src/components/common/Layout'
 
 function Data(): React.ReactElement {
-  return <div>hello world</div>
+  const data = React.useMemo(
+    () => [
+      {
+        col1: 'Hello',
+        col2: 'World',
+      },
+      {
+        col1: 'react-table',
+        col2: 'rocks',
+      },
+      {
+        col1: 'whatever',
+        col2: 'you want',
+      },
+    ],
+    []
+  )
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Column 1',
+        accessor: 'col1', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Column 2',
+        accessor: 'col2',
+      },
+    ],
+    []
+  )
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+  })
+
+  return (
+    <Layout>
+      <Table {...getTableProps}>
+        <TableHead>
+          {headerGroups.map((headerGroup) => (
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <TableCell {...column.getHeaderProps()}>{column.render('Header')}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableHead>
+        <TableBody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row)
+            return (
+              <TableRow {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <TableCell {...cell.getCellProps()}>{cell.render('Cell')}</TableCell>
+                ))}
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </Layout>
+  )
 }
 
 export default Data
