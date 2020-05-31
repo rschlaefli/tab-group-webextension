66740d9114a3b877e90a7b915fa3f6b4004e8885
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-31 16:34:47 +0200

    Prepare a new page for the data management

---
+++ src/data.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import withProxyStore from './lib/withProxyStore'
+import Data from './pages/Data'
+
+//load tailwind styling and reset css
+import './styles/tailwind.css'
+
+// import basic webextension styling
+import 'webext-base-css/webext-base.css'
+
+ReactDOM.render(<Data />, document.getElementById('root'))
---
+++ src/pages/Data.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Data(): React.ReactElement {
+  return <div>hello world</div>
+}
+
+export default Data
