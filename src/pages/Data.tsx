bfeb011c40852b427d1151b6245ad9dc3ff35217
Roland Schläfli <rolandschlaefli@gmail.com>
2020-06-02 17:17:21 +0200

    Add steps for data submission

--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -10,12 +10,19 @@ import {
   TableCell,
   TableBody,
   Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Button,
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { useDropzone } from 'react-dropzone'
 import Papa from 'papaparse'
 
 import Layout from '@src/components/common/Layout'
+import { Delete } from '@material-ui/icons'
+
+const STEPS = ['Introduction', 'Data Import (Statistics)', 'Data Import (Tab Events)', 'Submission']
 
 const useStyles = makeStyles({
   container: {
@@ -28,21 +35,6 @@ const useStyles = makeStyles({
 
 function Data(): React.ReactElement {
   const styles = useStyles()
-
-  const [csvData, setCsvData] = useState<any[]>([])
-
-  const onDrop = useCallback((acceptedFiles) => {
-    acceptedFiles.forEach((file: File) => {
-      Papa.parse(file, {
-        delimiter: ';',
-        worker: true,
-        header: true,
-        complete(result) {
-          setCsvData(result.data)
-        },
-      })
-    })
-  }, [])
 
   const columns = useMemo(
     () => [
@@ -90,6 +82,22 @@ function Data(): React.ReactElement {
     []
   )
 
+  const [activeStep, setActiveStep] = useState(0)
+  const [csvData, setCsvData] = useState<any[]>([])
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file: File) => {
+      Papa.parse(file, {
+        delimiter: ';',
+        worker: true,
+        header: true,
+        complete(result) {
+          setCsvData(result.data)
+        },
+      })
+    })
+  }, [])
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
@@ -100,40 +108,76 @@ function Data(): React.ReactElement {
   return (
     <Layout>
       <div className="p-4">
-        <div className="mb-1 border border-gray-100" {...getRootProps()}>
-          <input {...getInputProps()} />
-          {isDragActive ? (
-            <p>Drop the files here ...</p>
-          ) : (
-            <p>Drop some files here, or click to select files</p>
-          )}
-        </div>
+        {activeStep === 0 && (
+          <>
+            <ul>
+              <li>Some intro...</li>
+              <li>Choose level of data submission (BASIC/COMPLETE)?</li>
+              <li>Basic: only show next step for aggregate stats upload?</li>
+              <li>Complete: show step for tab events upload with censoring capabilities?</li>
+            </ul>
+          </>
+        )}
 
-        <TableContainer className={styles.container} component={Paper}>
-          <Table size="small" {...getTableProps}>
-            <TableHead>
-              {headerGroups.map((headerGroup) => (
-                <TableRow {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => (
-                    <TableCell {...column.getHeaderProps()}>{column.render('Header')}</TableCell>
+        {activeStep === 1 && (
+          <>
+            <div className="mb-1 border border-gray-100" {...getRootProps()}>
+              <input {...getInputProps()} />
+              {isDragActive ? (
+                <p>Drop the files here ...</p>
+              ) : (
+                <p>Drop some files here, or click to select files</p>
+              )}
+            </div>
+            <TableContainer className={styles.container} component={Paper}>
+              <Table size="small" {...getTableProps}>
+                <TableHead>
+                  {headerGroups.map((headerGroup) => (
+                    <TableRow {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        <TableCell {...column.getHeaderProps()}>
+                          {column.render('Header')}
+                        </TableCell>
+                      ))}
+                      <TableCell>Actions</TableCell>
+                    </TableRow>
                   ))}
-                </TableRow>
-              ))}
-            </TableHead>
-            <TableBody className={styles.tableBody} {...getTableBodyProps()}>
-              {rows.map((row) => {
-                prepareRow(row)
-                return (
-                  <TableRow {...row.getRowProps()}>
-                    {row.cells.map((cell) => (
-                      <TableCell {...cell.getCellProps()}>{cell.render('Cell')}</TableCell>
-                    ))}
-                  </TableRow>
-                )
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
+                </TableHead>
+                <TableBody className={styles.tableBody} {...getTableBodyProps()}>
+                  {rows.map((row) => {
+                    prepareRow(row)
+                    return (
+                      <TableRow {...row.getRowProps()}>
+                        {row.cells.map((cell) => (
+                          <TableCell {...cell.getCellProps()}>{cell.render('Cell')}</TableCell>
+                        ))}
+                        <TableCell>
+                          <Button>
+                            <Delete />
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    )
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </>
+        )}
+
+        {activeStep === 2 && (
+          <>(Optional) Upload of the more critical tab events data (switches etc.) </>
+        )}
+
+        {activeStep === 3 && <>Verify files and allow submission/upload/zip download, ...</>}
+
+        <Stepper activeStep={activeStep}>
+          {STEPS.map((label, ix) => (
+            <Step key={label} onClick={() => setActiveStep(ix)}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
       </div>
     </Layout>
   )
