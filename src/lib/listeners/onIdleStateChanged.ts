448964942208c7a10c7af9ffb600c11f5fa320e0
Roland Schläfli <rolandschlaefli@gmail.com>
2020-06-12 13:29:05 +0200

    Release v0.2.8

--- src/lib/listeners/onIdleStateChanged.ts
+++ src/lib/listeners/onIdleStateChanged.ts
@@ -3,6 +3,7 @@ import { TAB_ACTION } from '@src/types/Extension'
 
 export default function onIdleStateChanged(nativePort) {
   return function onIdleStateChangedListener([newState]: [string]): void {
+    console.log('[background] Idle state changed to ', newState)
     if (newState === 'idle' || newState === 'locked') {
       postNativeMessage(nativePort, {
         action: TAB_ACTION.PAUSE,
