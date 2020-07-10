73229cd6df4d131c3012a2fa8b01ec01c02eb717
Roland Schläfli <rolandschlaefli@gmail.com>
2020-07-10 15:48:04 +0200

    Fix: Setup fork-ts-checker

--- webpack.config.js
+++ webpack.config.js
@@ -218,7 +218,10 @@ module.exports = function (webpackEnv, _) {
     plugins: [
       isEnvProduction && new MiniCssExtractPlugin(),
       // fork a ts typechecker
-      isEnvDevelopment && new ForkTsCheckerWebpackPlugin({ eslint: true }),
+      isEnvDevelopment &&
+        new ForkTsCheckerWebpackPlugin({
+          eslint: { enabled: true, files: ['src/**/*.{js,ts,tsx}'] },
+        }),
       // automatically reload if a missing module is newly installed
       isEnvDevelopment && new WatchMissingNodeModulesPlugin('node_modules'),
       // bundle analysis
