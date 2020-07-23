01655177fca8855311a8c9c6fd667cc109615e66
Roland Schläfli <rolandschlaefli@gmail.com>
2020-07-23 22:46:17 +0200

    Allow opening tabs in groups by title

--- src/lib/listeners/onOmniboxEnter.ts
+++ src/lib/listeners/onOmniboxEnter.ts
@@ -1,7 +1,17 @@
 import { openTabGroupAlias } from '@src/state/tabGroups'
+import { performBrowserActionSafe } from '../utils'
 
 export default function onOmniboxEnter({ dispatch }) {
-  return function onOmniboxEnterListener(tabGroupId): void {
-    dispatch(openTabGroupAlias({ tabGroupId }))
+  return function onOmniboxEnterListener(action: string): void {
+    if (action.startsWith('group-open')) {
+      dispatch(openTabGroupAlias({ tabGroupId: action.replace('group-open-', '') }))
+    } else if (action.startsWith('tab-open')) {
+      performBrowserActionSafe(async (browser) => {
+        browser.tabs.update({
+          active: true,
+          url: action.replace('tab-open-', ''),
+        })
+      })
+    }
   }
 }
--- src/lib/listeners/onOmniboxInput.ts
+++ src/lib/listeners/onOmniboxInput.ts
@@ -1,18 +1,30 @@
-import { ITabGroup } from '@src/types/Extension'
+import { ITabGroup, ITab } from '@src/types/Extension'
 
 export default function onOmniboxInput({ getState }) {
   return function onOmniboxInputListener(omniInput, suggest): void {
-    const relevantGroups: ITabGroup[] = getState().tabGroups.filter((group: ITabGroup) =>
-      group.name.includes(omniInput)
+    const groups = getState().tabGroups
+
+    const relevantGroups: ITabGroup[] = groups.filter((group: ITabGroup) =>
+      group.name.toLowerCase().includes(omniInput)
     )
 
-    if (relevantGroups.length > 0) {
-      suggest(
-        relevantGroups.map((group) => ({
-          content: group.id,
-          description: `Open tab group: ${group.name}`,
-        }))
+    const relevantTabs: ITab[] = groups
+      .flatMap((group: ITabGroup) => group.tabs)
+      .filter(
+        (tab: ITab) =>
+          tab.title.toLowerCase().includes(omniInput) ||
+          tab.displayTitle?.toLowerCase().includes(omniInput)
       )
-    }
+
+    suggest([
+      ...relevantGroups.map((group) => ({
+        content: `group-open-${group.id}`,
+        description: `Open group: ${group.name}`,
+      })),
+      ...relevantTabs.map((tab) => ({
+        content: `tab-open-${tab.url}`,
+        description: `Open tab: ${tab.displayTitle || tab.title}`,
+      })),
+    ])
   }
 }
